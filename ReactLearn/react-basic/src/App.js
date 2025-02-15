15376ba34a75657367fcccfd1e09efdff5612960
August <2837904488qq.com>
2025-02-16 01:36:14 +0800

    要是假期都学完了开学学什么

--- ReactLearn/react-basic/src/App.js
+++ ReactLearn/react-basic/src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// const count = 100
+// function getName() {
+//   return '荷叶饭'
+// }
+// const list = [
+//   { id: 1001, name: 'Vue' },
+//   { id: 1002, name: 'React' },
+//   { id: 1003, name: 'Angular' }
+// ]
+// const isLogin=false
+// const articleType = 1//0,1,3
+// //定义核心函数：根据不同文章类型返回不同JSX模板
+// function getArticleTem() {
+//   if (articleType === 0) {
+//     return <div>我是无图文章</div>
+//   } else if (articleType === 1) {
+//     return <div>我是单图文章</div>
+//   } else {
+//     return <div>我是多图文章</div>
+//   }
+// }
+//定义组件
+function Button() {
+  //组件内部逻辑
+  return <button>click me!</button>
+}
 
 function App() {
+  // const handleClick = (name,e) => console.log('button被点击了name:',name,'e:',e)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*自闭和*/}
+      <Button />
+      {/*成对标签*/}
+      <Button></Button>
+
+      {/* <button onClick={(e)=>handleClick('荷叶饭',e)}>click me</button> */}
+
+      {/* {getArticleTem()} */}
+      {/* 逻辑与&&，短路运算符 */}
+      {/* {isLogin && <span>this is span</span>} */}
+      {/* {三元表达式} */}
+      {/* {isLogin?<span>荷叶饭</span>:<span>loading...</span>} */}
+
+      {/* <ul>
+        {list.map(item => <li key={item.id}>{item.name}</li>)}
+    </ul> */}
+
+      {/* {//使用引号传递字符串
+        'this is message'
+      }
+      {//识别JavaScript变量
+        count
+      }
+      {//函数调用
+        getName()
+      }
+      {//方法调用
+        new Date().getDate()
+      }
+      {//使用js对象
+        <div style={{ color: 'red' }}>this is div</div>
+      } */}
     </div>
   );
 }
--- ReactLearn/react-basic/src/index.js
+++ ReactLearn/react-basic/src/index.js
@@ -1,17 +1,8 @@
+//React必要的两个核心包
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//导入我们项目的根组件
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+//把App根组件渲染到id为root的Dom节点
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
